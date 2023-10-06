216d2ad4f58caa2cc56c89db2a51682c4eecfee4
Facundo <facundoprato@gmail.com>
2023-10-06 21:28:04 +0200

    arreglando routes

--- src/App.js
+++ src/App.js
@@ -1,41 +1,40 @@
 import React, { useState } from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { DatosProvider } from "../src/Context/DatosContext";
 import GlobalStyle from "./GlobalStyle";
 import { ThemeProvider } from "styled-components";
-import { temaClaro,temaOscuro } from "./Components/UI/Temas";
+import { temaClaro, temaOscuro } from "./Components/UI/Temas";
 import { Home } from "./Pages/Home";
 import { NuevoVideo } from "./Pages/NuevoVideo";
 import { NuevaCategoria } from "./Pages/NuevaCategoria";
 
 function App() {
-
-  const [tema,setTema]=useState(true);
+  const [tema, setTema] = useState(true);
 
   const toggleTema = () => {
-    setTema((tema) => !tema)
-  }
+    setTema((tema) => !tema);
+  };
 
   return (
     <>
-        <ThemeProvider theme={ tema ? temaClaro : temaOscuro}>
-          <GlobalStyle />
-          <DatosProvider>
-            <BrowserRouter>
-              <Routes>
-                  <Route index element={<Home />} />
-                  <Route path="nuevo-video" element={<NuevoVideo />} />
-                  <Route path="nueva-categoria" element={<NuevaCategoria />} />
-              </Routes>
-            </BrowserRouter>
-          </DatosProvider>
-        </ThemeProvider>
+      <ThemeProvider theme={tema ? temaClaro : temaOscuro}>
+        <GlobalStyle />
+        <DatosProvider>
+          <BrowserRouter>
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="nuevo-video" element={<NuevoVideo />} />
+              <Route path="nueva-categoria" element={<NuevaCategoria />} />
+            </Routes>
+          </BrowserRouter>
+        </DatosProvider>
+      </ThemeProvider>
     </>
   );
 }
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(<App />);
 
 export default App;
